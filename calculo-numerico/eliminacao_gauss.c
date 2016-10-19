3d4fb165276e3ae0fb213fc22f3593a58f4d0d34
Gustavo Oliveira <ovatsug_soul@hotmail.com>
2016-10-19 19:01:34 -0200

    Agora com pivotamento.

--- calculo-numerico/eliminacao_gauss.c
+++ calculo-numerico/eliminacao_gauss.c
@@ -1,160 +1,214 @@
 #include<stdio.h>
 
-/*
-variaveis para determinar o tamanho das matrizes do sistema.if (entrada == NULL)
-a funcao contar_linhas ira encontrar o tamanho da matriz no txt
-e alterar estas variaveis de acordo com o que tiver no txt
-*/
+int LIN, COL;
 
-int lin=0;
-int col=0;
 
-//imprimit matriz
-void i_matriz(float mat[lin][col]);
-//preencher matriz
-void p_matriz(float mat[lin][col]);
-//multiplicador da matriz
-float m_matriz(float mat[lin][col], int i, int j);
-//escalonar a matriz
-void e_matriz(float mat[lin][col],  int linha);
-//substituicao retroativa da matriz
-void s_r_matriz(float mat[lin][col]);
-//contar linhas do txt
-void contar_linhas();
-//imprime as respostas do sistema
-void p_resposta(float resp[lin]);
+//conto as linhas do txt e altero as variaveis globais
+int contar_linhas();
+//leio o conteudo do txt e armazeno na variavel mat
+void ler_matriz(float mat[LIN][COL]);
+//funcao para escalonar a matriz
+void escalonar(float mat[LIN][COL], int linha);
+//funcao para imprimir a matriz
+void imprimir(float mat[LIN][COL]);
+//funcao para mudar as linhas da matriz caso o pivo seja 0 (para evitar indeterminacao do tipo 0/0)
+void mudar_linhas(float mat[LIN][COL], int linha);
+//funcao para encontrar o maior elemento para que seja feita a mudanca das linhas
+int maior_elem(float mat[LIN][COL], int linha);
+//funcao para substituicao retroativa da matriz escalonada
+void s_r_matriz(float mat[LIN][COL]);
+//funcao para imprimir o vetor de resposta da matriz
+void p_resposta(float resp[LIN]);
 
-int main(){
+void main(){
+	if(contar_linhas()){
 
-	/*
-	executo esta funcao antes de tudo, pois a primeira coisa a se fazer
-	eh determinar o tamanho da minha matriz
-	*/
-	contar_linhas();
-	float matriz[lin][col];
-	float mult;
+		int i;
 
-	p_matriz(matriz);
-	printf("Matriz original: \n");
-	i_matriz(matriz);
-	for(int i=0;i<lin;i++){
-		e_matriz(matriz, i);
+		contar_linhas();
+		float mat[LIN][COL];
+		//mat -> var principal, pois faco todas as operacoes de escalonamento nesta matriz
+		ler_matriz(mat);
+		
+		printf("Matriz Original\n");
+		imprimir(mat);
+
+		for(i=0;i<COL;i++){
+			//caso o pivo seja 0, chamo a funcao para mudar a linha para o maior valor das outras linhas de mesma coluana
+			if(mat[i][i] == 0)
+				mudar_linhas(mat,i);
+
+			escalonar(mat,i);
+		}
+		printf("Matriz Escalonada\n");
+		imprimir(mat);
+		s_r_matriz(mat);
+	}else{
+		printf("Arquivo n encontrado!\n");
 	}
-
-	printf("\nMatriz Escalonada: \n");
-	i_matriz(matriz);
-	//printf("LOL:\n");
-	s_r_matriz(matriz);
-	return 0;
 }
 
+int contar_linhas(){
 
-//imprime matriz
-void i_matriz(float mat[lin][col]){
+	//abro o arquivo
+	FILE *arq;
+	arq = fopen("matriz.txt", "r");
+	//variavel para armazenar a qnt de linhas do txt
+	//tomo como base as linhas do txt para dizer a ordem do sistema
+	if(arq != NULL){
+		int linhas=0;
 
-	for(int i=0;i<lin;i++){
-		for(int j=0; j<col;j++){
+		//percorro todo o txt
+		while(!feof(arq)){
+		//feof retorna 1 quando o arquivo chega ao final, por isso nego
+			if(fgetc(arq) == '\n'){
+			//quando encontro um \n significa que vou para uma nova linha do txt
+				linhas++;
+			}
+		}
+
+		//fecho o arquivo
+		fclose(arq);
+
+		/*
+		altero o valor das variaveis globais
+		e por consequencia altero o tamanho das matrizes do programa
+		de acordo com o tamanho da matriz lida no txt
+		*/
+		
+		// LIN = linhas;
+		// COL = linhas + 1;
+
+		LIN = linhas + 1;
+		COL = LIN + 1;
+		return 1;
+	}else{
+		return 0;
+	}
+}
+
+//le o conteudo de um txt
+void ler_matriz(float mat[LIN][COL]){
+
+	FILE *arq;
+	arq = fopen("matriz.txt", "r");
+
+	for(int i=0;i<LIN;i++){
+		for(int j=0; j<COL;j++){
+			fscanf(arq, "%f", &mat[i][j]);
+		}
+	}
+	fclose(arq);
+}
+
+void imprimir(float mat[LIN][COL]){
+	for(int i=0;i<LIN;i++){
+		for(int j=0; j<COL;j++){
 			printf("%.2f ", mat[i][j]);
 		}
 		printf("\n");
 	}
 }
 
-//preenche matriz
-void p_matriz(float mat[lin][col]){
-
-	FILE *arq;
-	arq = fopen("matriz.txt", "r");
-
-	for(int i=0;i<lin;i++){
-		for(int j=0; j<col;j++){
-			fscanf(arq, "%f", &mat[i][j]);
-		}
-	}
-
-	fclose(arq);
-
-	contar_linhas();
-	printf("Ordem da matriz: %d!\n", lin);
-
-}
-
-//multiplicador matriz
-//i -> linha
-//j -> coluna
-float m_matriz(float mat[lin][col], int i, int j){
-
-	float m;
-
-	if(i == (lin-1)){
-		/*
-		caso i == TAM-1, significa que estou na ultuma linha da matriz
-		entao retorno o proprio i, pois vou dividir o i pelo proprio i
-		e obter a ultima linha sem alteracao
-		*/
-		m = i;
-	}else if(mat[j][i] != 0){
-		/*
-		preciso garantir que a mat[j][i] seja diferente de zero
-		para que nao haja indeterminacao do tipo 0/0.
-		*/
-		m = mat[j][i] / mat[i][i];
-	}else{
-		//caso chegue aqui significa que o pivo eh 0
-		m = 0;
-	}
-	return m;
-}
-
-
-//escalonamento da matriz
-void e_matriz(float mat[lin][col],  int linha){
-
-	float vet[col];
+//segundo parametro referente a linha/pivo que estou escalonando
+void escalonar(float mat[LIN][COL], int linha){
+	int i,j;
 	float mult;
 
-	for(int i=linha+1;i<lin;i++){
-		//armazeno o multiplicador da linha em mult
-		mult = m_matriz(mat,linha, i);
-		for(int j=0;j<col;j++){
-			//o vet recebe o (mult*-1) * (a linha do pivo)
-			vet[j] = (-1*mult)*mat[linha][j];
-			//somo linha de destino com o vet
-			mat[i][j] += vet[j];
+	for(i=linha+1;i<LIN;i++){
+		//mult = linha atua / pivo
+		mult = mat[i][linha] / mat[linha][linha];
+		for(j=0;j<COL;j++){
+			//linha atual = linha atual -(mult) * (linha do pivo, coluna j)
+			mat[i][j] = mat[i][j]-mult*mat[linha][j];
 		}
-//		printf("\n");
 	}
 }
 
-void s_r_matriz(float mat[lin][col]){
+void mudar_linhas(float mat[LIN][COL], int linha){
+	int i,aux;
+	//i -> contador
+	//aux -> var para auxiliar na troca dos elementos da linha
 
-	float resp[lin];
+	int pivo = maior_elem(mat,linha);
+	//pivo recebe o indice da linha em que o maior elemento esta
+	//retorno 0 caso nao haja mudanca de linhas
 
-	//inicializo o vetor das respostas com 1 em todas as posicoes
-	for(int i=0;i<lin;i++){
+	if(pivo){
+		for(i=0;i<COL;i++){
+			//algoritmo de troca de linhas da matriz basica
+			aux = mat[linha][i];
+			mat[linha][i] = mat[pivo][i];
+			mat[pivo][i] = aux;
+		}
+	}
+}
+
+int maior_elem(float mat[LIN][COL], int linha){
+	int i,j,indice_m=-1;
+	//inicializo indice_m com -1 para ter controle da mudanca de valor da variavel
+	//retorno 0 caso ela n seja alterada
+
+	float m;
+	//m -> maior valor
+
+	for(i=linha;i<LIN;i++){
+		if(i==0){
+		//caso seja a primeira execucao assumo mat[linha][linha] como o maior elemento
+			m = mat[i][linha];
+			indice_m = i;
+		}else if(mat[i][linha] > m){
+			m = mat[i][linha];
+			indice_m = i;
+		}
+	}
+
+	if(indice_m == -1){
+		return 0;
+	}else{
+		return indice_m;
+	}
+}
+
+void s_r_matriz(float mat[LIN][COL]){
+
+	//vetor para guardar as respostas
+	float resp[LIN];
+	int i,j;
+
+	/*
+	inicializo o vetor das respostas com 1 em todas as posicoes
+	pois o for abaixo multiplica cada posicao da matriz pela mesma
+	posicao do vetor respostas, caso a posicao seja 1 o valor nao
+	sera alterado
+	*/
+	for(i=0;i<LIN;i++){
 		resp[i] = 1;
 	}
 
-	//vetor para guardar as respostas
+	//aux garda a soma dos elementos da LINha i da matriz
 	float aux=0;
-	int i,j;
 
-	printf("\n");
-	for(i=lin-1;i>=0;i--){
-		for(j=col-1;j>=0;j--){
-			if(j==(col-1)){
-				//j == col-1 significa que estou no
+	//inicializo o for de baixo para cima
+	for(i=LIN-1;i>=0;i--){
+		for(j=COL-1;j>=0;j--){
+			if(j==(COL-1)){
+				//j == COL-1 significa que estou no
 				//termo independente
 				//entao apenas somo.
 				aux += mat[i][j];
 			}else if(i!=j){
 				//somo o inverso de todos os numeros
-				//com a excessao do pivo
+				//com excessao do pivo por isso do i != j
 				aux += (-1*mat[i][j]) * (resp[j]);
 			}
 		}
-		//divido o acumulador pelo valor do pivo
-		aux /= mat[i][i];
+		//divido aux pelo valor do pivo
+		if(mat[i][i] == 0){
+			aux =0;
+		}else{
+			aux /= mat[i][i];
+		}
 		//armazeno o resultado no vetor de respostas
 		resp[i] = aux;
 		//zero o acumulador
@@ -164,42 +218,14 @@ void s_r_matriz(float mat[lin][col]){
 	p_resposta(resp);
 }
 
-void p_resposta(float resp[lin]){
+void p_resposta(float resp[LIN]){
 
 	int i=0;
 
-	printf("Respostas\n");
-	for(i=0;i<lin;i++){
+	printf("\nRespostas\n");
+	for(i=0;i<LIN;i++){
+		//imprimo o vetor de resposta
 		printf("x[%d] = %.2f\n", i+1,resp[i]);
 	}
-}
 
-void contar_linhas(){
-
-	//abro o arquivo
-	FILE *arq;
-	arq = fopen("matriz.txt", "r");
-	//variavel para armazenar a qnt de linhas do txt
-	//tomo como base as linhas do txt para dizer a ordem do sistema
-	int linhas=0;
-
-	//percorro todo o txt
-	while(!feof(arq)){
-	//feof retorna true quando o arquivo chega ao final
-		if(fgetc(arq) == '\n'){
-		//quando encontro um \n significa que vou para uma nova linha do txt
-			linhas++;
-		}
-	}
-
-	//fecho o arquivo
-	fclose(arq);
-
-	/*
-	altero o valor das variaveis globais
-	e por consequencia altero o tamanho das matrizes do programa
-	de acordo com o tamanho da matriz lida no txt
-	*/
-	lin = linhas;
-	col = linhas + 1;
 }
